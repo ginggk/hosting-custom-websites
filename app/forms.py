aca16337b07622c6755d30e3358ed23d24d1c79c
gingerkeys <gkeys@basecampcodingacademy.org>
2019-04-24 11:28:09 -0500

    added view functions that render the css template in a seperate file instead of in the same file

--- app/forms.py
+++ app/forms.py
@@ -3,3 +3,15 @@ from django import forms
 
 class InformationForm(forms.Form):
     name = forms.CharField()
+    personal_icon = forms.URLField()
+    color = forms.CharField()
+    banner_picture = forms.URLField()
+    pic1 = forms.URLField()
+    pic2 = forms.URLField()
+    pic3 = forms.URLField()
+    bio = forms.CharField()
+    twitter = forms.URLField()
+    facebook = forms.URLField()
+    instagram = forms.URLField()
+    github = forms.URLField()
+    email = forms.EmailField()
--- app/views.py
+++ app/views.py
@@ -24,14 +24,30 @@ def build_zip(form):
 
     html_template = get_template('index.html')
     html_context = {
-        'name': form.cleaned_data['name']
+        'name': form.cleaned_data['name'],
+        'personal_icon': form.cleaned_data['personal_icon'],
+        'banner_picture': form.cleaned_data['banner_picture'],
+        'pic1': form.cleaned_data['pic1'],
+        'pic2': form.cleaned_data['pic2'],
+        'pic3': form.cleaned_data['pic3'],
+        'bio': form.cleaned_data['bio'],
+        'twitter': form.cleaned_data['twitter'],
+        'facebook': form.cleaned_data['facebook'],
+        'instagram': form.cleaned_data['instagram'],
+        'github': form.cleaned_data['github'],
+        'email': form.cleaned_data['email'],
+        'color': form.cleaned_data['color']
         # form data
     }
 
+    css_template = get_template('style.css')
+    css_context = {'color': form.cleaned_data['color']}
+
     html_rendered_template = html_template.render(html_context)
+    css_rendered_template = css_template.render(css_context)
 
     zip.writestr("index.html", html_rendered_template)
-    # zip.writestr("styles.css", css_rendered_template)
+    zip.writestr("style.css", css_rendered_template)
 
     # fix for Linux zip files read in Windows
     for file in zip.filelist:
@@ -54,3 +70,9 @@ class WebsiteInfo(View):
         if form.is_valid():
             zip_string = build_zip(form)
             return ZipResponse(zip_string, "your_fancy_new_website")
+        else:
+            print(form.is_valid())
+            print(form.cleaned_data)
+            print(form.errors)
+            return render(request, 'information.html',
+                          {'InformationForm': form})
